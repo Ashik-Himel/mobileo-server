3a81cdcdd9d4ab72de842094da399a5f0bc14633
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-10-31 19:11:20 +0600

    types and brands api added

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xeaidsx.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb://127.0.0.1:27017";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database = client.db('mobileo');
+    const typeCollection = database.collection('types');
+    const brandCollection = database.collection('brands');
+
+    app.get('/types', async(req, res) => {
+      const options = {projection: {_id: 0, name: 1, icon: 1}};
+      const result = await typeCollection.find({}, options).toArray();
+      res.send(result);
+    })
+    app.get('/brands', async(req, res) => {
+      const options = {projection: {_id: 0, name: 1, icon: 1, thumbnail: 1}};
+      const result = await brandCollection.find({}, options).toArray();
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB Connected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to MobileO's server!");
+})
+app.listen(port, () => {
+  console.log(`Server is running in http://localhost:${port}`);
+})
+
+module.exports = app;
